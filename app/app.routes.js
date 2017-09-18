5e80b133be3eb3a6d3c34430d07ceeaa8ea59b64
NicolasOuld <ouldbouamama.nicolas@gmail.com>
2017-09-18 22:59:32 +0200

    Initialisation du projet

---
+++ app/app.routes.js
@@ -0,0 +1,21 @@
+'use strict';
+
+angular.module('challengeApp').config(
+    [
+        '$routeProvider',
+
+        function ($routeProvider) {
+          //$routeProvider.when('/homepage', {
+             //templateUrl: 'partial/homepage.html',
+             //controller: HomePageCtrl
+           //}).when('/users', {
+             //templateUrl: 'partial/users.html',
+             //controller: UsersListCtrl
+           //}).when('/contacts',{
+             //templateUrl: 'partial/contacts.html',
+             //controller: ContactPageCtrl
+           //}).otherwise({
+            // redirectTo: 'homepage'
+           //});
+        }
+    ]);
